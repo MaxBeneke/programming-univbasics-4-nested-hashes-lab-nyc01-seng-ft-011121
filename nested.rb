6e54e6827a1c7937b237ec1c2e48711c3d35ea3c
Max Beneke <max.beneke@gmail.com>
2020-12-08 21:00:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,8 +88,6 @@ programmer_hash[:yukihiro_matsumoto] = {
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -106,7 +104,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
